6339b83f9590ede3f8c693dfca983a0a4601ec6e
Abhinav_Jha <abhi.mount.26@gmail.com>
2021-01-16 20:57:55 +0530

    fixed some crap

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 const express = require('express')
 const path = require('path')
-const hbs = require("hbs")
-const home = require("./routers/home")
+const hbs = require('hbs')
+const home = require('./routers/home')
 const bodyParser = require('body-parser')
 
 const app = express()
 
-
 const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, "../public")
+const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialPath = path.join(__dirname, '../templates/partials')
 
-app.use(express.json({
-    limit: "1mb"
-}))
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+app.use(
+ express.json({
+  limit: '1mb',
+ }),
+)
+app.use(
+ bodyParser.urlencoded({
+  extended: false,
+ }),
+)
 app.use(express.static(publicDirectoryPath))
 app.set('view engine', 'hbs')
-app.set("views", viewsPath)
+app.set('views', viewsPath)
 hbs.registerPartials(partialPath)
 app.use(home)
 
-
-
 app.listen(port, () => {
-    console.log("Server Up At-:", port)
+ console.log('Server Up At-:', port)
 })
--- src/routers/home.js
+++ src/routers/home.js
@@ -1,83 +1,79 @@
 const express = require('express')
-    //const path = require('path')
+//const path = require('path')
 const router = express.Router()
 const nodemailer = require('nodemailer')
-require("dotenv").config()
-    //const scss = require("node-sass")
+require('dotenv').config()
+//const scss = require("node-sass")
 module.exports = router
 
-router.get("/", (req, res) => {
-    res.render("home") //----->> OR---->> res.render("./home.hbs")
+router.get('/', (_, res) => {
+ res.render('home') //----->> OR---->> res.render("./home.hbs")
 })
 
-router.get('/skills', (req, res) => {
-    res.render("skills")
+router.get('/skills', (_, res) => {
+ res.render('skills')
 })
-router.get('/education', (req, res) => {
-    res.render("education")
+router.get('/education', (_, res) => {
+ res.render('education')
 })
-router.get('/quotes', (req, res) => {
-    // const quoteStyle = path.join(__dirname, "../../public/sass/quotes.scss")
-    // scss.render({
-    //     file: quoteStyle,
-    //     outFile: path.join(__dirname, "../../public/css/quotes.css")
-    // })
-    // const quotePath = path.join(__dirname, "../../templates/views/quotes.html")
-    // res.sendFile(quotePath)
-    res.render("quotes")
-})
-
-
-router.get("/chatapp", (req, res) => {
-    res.render("chatapp")
+router.get('/quotes', (_, res) => {
+ // const quoteStyle = path.join(__dirname, "../../public/sass/quotes.scss")
+ // scss.render({
+ //     file: quoteStyle,
+ //     outFile: path.join(__dirname, "../../public/css/quotes.css")
+ // })
+ // const quotePath = path.join(__dirname, "../../templates/views/quotes.html")
+ // res.sendFile(quotePath)
+ res.render('quotes')
 })
 
-router.get('/darkskyapp', (req, res) => {
-    res.render("darkskyapp")
-
+router.get('/chatapp', (_, res) => {
+ res.render('chatapp')
 })
-router.get('/expensehandler', (req, res) => {
-    res.render("expensehandler")
+
+router.get('/darkskyapp', (_, res) => {
+ res.render('darkskyapp')
+})
+router.get('/expensehandler', (_, res) => {
+ res.render('expensehandler')
 })
 
 router.post('/', (req, res) => {
+ let { name, email, subject, message } = req.body
+ name = name.toLowerCase()
+ email = email.toLowerCase()
+ subject = subject.toLowerCase()
 
-    let { name, email, subject, message } = req.body
-    name = name.toLowerCase()
-    email = email.toLowerCase()
-    subject = subject.toLowerCase()
+ const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+   user: process.env.USER,
+   pass: process.env.PASSWORD,
+  },
+ })
 
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.USER,
-            pass: process.env.PASSWORD
-        }
-    });
+ const mailOptions = {
+  from: 'abhiinvirtuallife@gmail.com',
+  to: 'abhi.mount.26@gmail.com',
+  subject: subject,
+  text: `Name-:${name} E-mail-: ${email}
+        Message-: ${message} `,
+ }
 
-    const mailOptions = {
-        from: 'abhiinvirtuallife@gmail.com',
-        to: 'abhi.mount.26@gmail.com',
-        subject: subject,
-        text: `Name-:${name} E-mail-: ${email}
-        Message-: ${message} `
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error)
-            res.render('./home.hbs', {
-                data: "Error! Message Not Sent."
-            })
-        } else {
-            res.render('./home.hbs', {
-                data: "Message Sent!"
-            })
-        }
-    });
-
+ transporter.sendMail(mailOptions, (error, _) => {
+  if (error) {
+   console.log(error)
+   res.render('./home.hbs', {
+    data: 'Error! Message Not Sent.',
+   })
+  } else {
+   res.render('./home.hbs', {
+    data: 'Message Sent!',
+   })
+  }
+ })
 })
 
-router.get("/*", (req, res) => {
-    res.render("./error.hbs")
+router.get('/*', (_, res) => {
+ res.render('./error.hbs')
 })
